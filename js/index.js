ba9a8f37877c0145ee60ac4a5db40ade7574eae6
JenyaCherny <jenya.cherny@gmail.com>
2025-01-18 15:26:46 -0500

    completed all experiments and commented them out

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+let numbers = [125, 135, 200, 300]
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+
+let words = ['bird', 'lettuce', 'pear', 'screen', 'car'];
+for (let word of words) {
+    console.log(word);
+} 
+
+const veggies = ['cucumber', 'tomatoe', 'kale'];
+veggies.forEach(function(veggies, index) {
+    console.log(index + ': ' + veggies);
+});
+
+//let numbers = [10, 20, 30, 40, 50];
+for (let number of numbers) {
+    console.log(number+10);
+}
+
+*/
